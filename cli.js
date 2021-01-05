c8011722c75628071d2050e89e154f5c435b1941
alestor123 <alestoraldous@gmail.com>
2021-01-05 18:40:00 +0530

    Added bash

--- cli.js
+++ cli.js
@@ -1,3 +1,7 @@
-var commit = require('./App');
+#!/usr/bin/env node
 
-commit('cli','*/1 * * * *')
+var commit = require('./App'),
+options = require('minimist')(process.argv.slice(2)),
+message = options.m || options.message,
+cron = options.c || options.cron || '*/5 * * * *';
+commit(message,cron)
