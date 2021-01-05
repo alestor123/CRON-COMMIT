dc0f253050340bd144a17aa9499ebff44c52ad72
alestor123 <alestoraldous@gmail.com>
2021-01-05 18:44:00 +0530

    working on err

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ ncron = require('node-cron');
 module.exports = (message,cron) => {
     if(!cron) throw Error('Cron Not Found')
     ncron.schedule(cron, ()=> {
-        exec(`git add . && git commit -m '${message||' commit message '}'`, (err) => {
-            if(err) throw err
-            })
+        exec(`git add . && git commit -m '${message||' commit message '}'`)
       });
 }
